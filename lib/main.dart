0ce83e058de66a2cf09f54d4182ac28c6e21b904
Karim <karim.mendes@gmail.com>
2020-09-28 21:55:18 -0300

    Alterado o tamanho do listtile

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:travelapp/app_bar_home.dart';
+import 'package:travelapp/restaurants/restaurants_home_cards.dart';
 
 import 'package:travelapp/stories_home_card.dart';
 import 'package:travelapp/stories_home_text.dart';
@@ -47,60 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
             HomeText("Atrações em Destaque"),
             TopHomeCard(),
             HomeText("Bares e Restaurantes"),
-            Container(
-              child: ListView.builder(
-
-                shrinkWrap: true,
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  return Container(
-                    height: 120,
-                    child: Card(
-                      elevation: 3,
-                      child: Row(
-                        children: <Widget>[
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: <Widget>[
-                              Image.asset(
-                                "assets/restaurante1.jpg",
-                                height: 100,
-                              ),
-                            ],
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    left: 15.0, bottom: 10),
-                                child: Text(
-                                  "Hangoo Restaurante",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w800,
-                                      fontSize: 16),
-                                ),
-                              ),
-                              SizedBox(
-                                width: 200,
-                                child: ListTile(
-                                  dense: true,
-                                  title: Text("Buffet Livre"),
-                                  subtitle: Text(
-                                      "Churrasco, Pizzas, Sobremesas, Sorvetes"),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
+            RestaurantsHomeCards(),
           ],
         )),
       ),
