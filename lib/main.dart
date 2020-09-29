a124c5190bbd9ea5f71b1cb1f64b7056952312ab
Karim <karim.mendes@gmail.com>
2020-09-28 21:12:45 -0300

    Incluido o SingleChildScrollView no scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -38,79 +38,71 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Container(
         padding: EdgeInsets.only(left: 10, right: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
+        child: SingleChildScrollView(
+            child: Column(
           children: <Widget>[
-            Expanded(
-                child: ListView(
-                  shrinkWrap: true,
-              children: <Widget>[
-                FilterHome(),
-                StoriesHomeText(),
-                StoriesHomeCard(),
-                HomeText("Atrações em Destaque"),
-                TopHomeCard(),
-                HomeText("Bares e Restaurantes"),
-                Container(
-                  height: 400,
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    itemCount: 4,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        height: 120,
-                        child: Card(
-                          elevation: 3,
-                          child: Row(
-                            children: <Widget>[
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  Image.asset(
-                                    "assets/restaurante1.jpg",
-                                    height: 100,
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 15.0, bottom: 10),
-                                    child: Text(
-                                      "Hangoo Restaurante",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w800,
-                                          fontSize: 16),
-                                    ),
-                                  ),
-                                  SizedBox(
+            FilterHome(),
+            StoriesHomeText(),
+            StoriesHomeCard(),
+            HomeText("Atrações em Destaque"),
+            TopHomeCard(),
+            HomeText("Bares e Restaurantes"),
+            Container(
+              child: ListView.builder(
 
-                                    width: 200,
-                                    child: ListTile(
-                                      dense: true,
-                                      title: Text("Buffet Livre"),
-                                      subtitle:
-                                          Text("Churrasco, Pizzas, Sobremesas, Sorvetes"),
-                                    ),
-                                  ),
-                                ],
+                shrinkWrap: true,
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 120,
+                    child: Card(
+                      elevation: 3,
+                      child: Row(
+                        children: <Widget>[
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: <Widget>[
+                              Image.asset(
+                                "assets/restaurante1.jpg",
+                                height: 100,
                               ),
                             ],
                           ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
-            )),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 15.0, bottom: 10),
+                                child: Text(
+                                  "Hangoo Restaurante",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 16),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 200,
+                                child: ListTile(
+                                  dense: true,
+                                  title: Text("Buffet Livre"),
+                                  subtitle: Text(
+                                      "Churrasco, Pizzas, Sobremesas, Sorvetes"),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
-        ),
+        )),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
