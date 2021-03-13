6574ed18f3c68276197737ee2fea91072d3bb266
Karim <karim.mendes@gmail.com>
2021-03-13 20:04:47 -0300

    Add maps

--- lib/app/ui/theme/utils.dart
+++ lib/app/ui/theme/utils.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Util{
 
   static Color background = Color.fromRGBO(242, 241, 243, 1);
   static Color topText = Color.fromRGBO(54, 71, 88, 1);
+  static List<Color> colors = [Colors.blue, Colors.green, Colors.cyan,Colors.teal, Colors.orange];
+  static List<double> marker = [BitmapDescriptor.hueBlue, BitmapDescriptor.hueGreen, BitmapDescriptor.hueCyan,BitmapDescriptor.hueOrange];
+  static String googleApi = "AIzaSyB2ULKb68xiXu25e0qh0CUNzBSCI622R0c";
+
 
 }
--- lib/map.page.dart
+++ lib/map.page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:travelapp/app/data/controller/attraction_controller.dart';
@@ -23,22 +24,36 @@ class _MapPageState extends State<MapPage> {
   GoogleMapController mapController;
   Set<Marker> markers = new Set<Marker>();
 
+  PolylinePoints polylinePoints;
+  List<LatLng> polylineCoordinates = [];
+  Map<PolylineId, Polyline> polylines = {};
+
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
   @override
   Widget build(BuildContext context) {
-    GeoPoint geoPoint = widget.geoPoint;
-    String description = widget.description;
-    LatLng position = LatLng(geoPoint.latitude, geoPoint.longitude);
-    final Marker marker = Marker(
-        markerId: new MarkerId("1213"),
-        position: position,
-        infoWindow: InfoWindow(title: description));
-    setState(() {
+     GeoPoint initialGeoPoint = widget.geoPoint;
+     int x = 0;
+    for (var att in attractionController.attractionsList) {
+      GeoPoint geoPoint = att.geoPoint;
+      String description = att.title;
+      LatLng position = LatLng(geoPoint.latitude, geoPoint.longitude);
+      final Marker marker = Marker(
+          markerId: new MarkerId(att.reference.id),
+          position: position,
+          icon: BitmapDescriptor.defaultMarkerWithHue(Util.marker[x]),
+          infoWindow: InfoWindow(title: description));
+      //setState(() {
+      x++;
       markers.add(marker);
-    });
+      //});
+
+    }
+
+    //TODO essa porra é paga
+     //(attractionController.attractionsList[0].geoPoint,attractionController.attractionsList[1].geoPoint);
 
     return Scaffold(
         appBar: AppBar(
@@ -51,27 +66,24 @@ class _MapPageState extends State<MapPage> {
               Expanded(
                 flex: 6,
                 child: GoogleMap(
+                  polylines: Set<Polyline>.of(polylines.values),
                   markers: markers,
                   onMapCreated: _onMapCreated,
                   initialCameraPosition:
-                      CameraPosition(target: LatLng(geoPoint.latitude, geoPoint.longitude), zoom: 15.0),
+                      CameraPosition(target: LatLng(initialGeoPoint.latitude, initialGeoPoint.longitude), zoom: 12),
                 ),
               ),
               Expanded(
                 flex: 4,
                 child: Container(
-                  color: Colors.blue,
-                  child: Column(
-                    children: [
-                      buildLocations(
-                          Colors.deepOrangeAccent, "Museuu da Água", false),
-                      buildLocations(
-                          Colors.blue, "Rua das Palmeiras", false),
-                      buildLocations(
-                          Colors.deepPurple, "Museu da Esquina", false),
-                      buildLocations(Colors.teal, "Teatro da Paz", false),
-                    ],
-                  ),
+                  color: Util.background,
+                  child: ListView.builder(
+                    itemCount: attractionController.attractionsList.length,
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      return buildLocations(Util.colors[index], attractionController.attractionsList[index].title, true);
+                    }
+                  )
                 ),
               ),
             ],
@@ -79,41 +91,74 @@ class _MapPageState extends State<MapPage> {
         ));
   }
 
-  Expanded buildLocations(Color color, String text, bool fav) {
-    return Expanded(
-        flex: 2,
-        child: Container(
-          color: Util.background,
-          child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                      flex: 1,
-                      child: Icon(
-                        Icons.location_on,
-                        color: color,
-                        size: 30,
-                      )),
-                  Expanded(
-                      flex: 6,
-                      child: Text(
-                        text,
-                      )),
-                  Expanded(
-                      flex: 2,
-                      child: Icon(
-                        Icons.check_box,
-                        color: color,
-                      )),
-                  Expanded(
-                      flex: 1,
-                      child: Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                      )),
-                ],
-              )),
-        ));
+  Padding buildLocations(Color color, String text, bool fav) {
+    return
+        Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                    flex: 10,
+                    child: Icon(
+                      Icons.location_on,
+                      color: color,
+                      size: 30,
+                    )),
+                Expanded(
+                    flex: 60,
+                    child: Text(
+                      text,
+                    )),
+                Expanded(
+                    flex: 20,
+                    child: Icon(
+                      Icons.check_box,
+                      color: color,
+                    )),
+                Expanded(
+                    flex: 10,
+                    child: Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    )),
+              ],
+            ));
+  }
+
+  // Create the polylines for showing the route between two places
+
+  _createPolylines(GeoPoint start, GeoPoint destination) async {
+    // Initializing PolylinePoints
+    polylinePoints = PolylinePoints();
+
+    // Generating the list of coordinates to be used for
+    // drawing the polylines
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      Util.googleApi, // Google Maps API Key
+      PointLatLng(start.latitude, start.longitude),
+      PointLatLng(destination.latitude, destination.longitude),
+      travelMode: TravelMode.transit,
+    );
+
+    // Adding the coordinates to the list
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      });
+    }
+
+    // Defining an ID
+    PolylineId id = PolylineId('poly');
+
+    // Initializing Polyline
+    Polyline polyline = Polyline(
+      polylineId: id,
+      color: Colors.red,
+      points: polylineCoordinates,
+      width: 3,
+    );
+
+    // Adding the polyline to the map
+    polylines[id] = polyline;
   }
 }
